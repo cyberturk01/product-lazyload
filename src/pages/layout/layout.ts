916e7fbb5da2903cfa6571a1e620e1504f336f80
Gokhan Yigit <yigitgokhan1@gmail.com>
2025-07-13 19:29:43 +0200

    Add todo app component

--- src/pages/layout/layout.ts
+++ src/pages/layout/layout.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Navbar } from './navbar/navbar';
 import { Sidebar } from './sidebar/sidebar';
 import { RouterOutlet } from '@angular/router';
+import { Todo } from '../todo/todo';
 
 @Component({
   selector: 'app-layout',
-  imports: [Navbar, Sidebar, RouterOutlet],
+  imports: [Navbar, Sidebar, Todo, RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
---
+++ src/pages/todo/todo.ts
@@ -0,0 +1,87 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TodoPipe } from '../todo-pipe';
+
+@Component({
+  selector: 'app-todo',
+  imports: [FormsModule, CommonModule, TodoPipe],
+  templateUrl: './todo.html',
+  styleUrl: './todo.css',
+})
+export class Todo {
+  isUpdated: boolean = false;
+  search: string = '';
+  searchSomething: string = 'Search Something:';
+  updateIndex: number = 0;
+  updateWork: string = '';
+  // todos: string[] = ['Tomate', 'Pears', 'Apple'];
+  // work: string = '';
+  // date: Date | undefined;
+  // price: number = 123412;
+
+  // enterWork(event: any) {
+  //   this.work = event.target.value;
+  // }
+  // save() {
+  //   this.todos.push(this.work);
+  //   this.work = '';
+  // }
+  // delete(index: number) {
+  //   this.todos.splice(index, 1);
+  // }
+  // get(index: number) {
+  //   this.updateIndex = index;
+  //   this.updateWork = this.todos[index];
+  //   this.isUpdated = true;
+  // }
+  // update() {
+  //   this.todos[this.updateIndex] = this.updateWork;
+  //   this.updateWork = '';
+  //   this.isUpdated = false;
+  // }
+  todos: { work: string; date?: Date; price: number }[] = [
+    { work: 'Tomate', date: this.getDate(), price: 123412 },
+    { work: 'Pears', date: this.getDate(), price: 123412 },
+    { work: 'Apple', date: this.getDate(), price: 123412 },
+  ];
+
+  work: string = '';
+  date: Date = this.getDate();
+  price: number = 123412;
+
+  // updateIndex: number = -1;
+  // updateWork: string = '';
+  // isUpdated: boolean = false;
+
+  enterWork(event: any) {
+    this.work = event.target.value;
+  }
+
+  save() {
+    this.todos.push({ work: this.work, date: this.date, price: this.price });
+    this.work = '';
+    this.date = this.getDate();
+    this.price = 123412;
+  }
+
+  delete(index: number) {
+    this.todos.splice(index, 1);
+  }
+
+  get(index: number) {
+    this.updateIndex = index;
+    this.updateWork = this.todos[index].work;
+    this.isUpdated = true;
+  }
+
+  update() {
+    this.todos[this.updateIndex].work = this.updateWork;
+    this.updateWork = '';
+    this.isUpdated = false;
+  }
+
+  getDate() {
+    return (this.date = new Date());
+  }
+}
